1857c0095f4d302c6167eb07f40e05b61d3faeec
unknown <darrylandrew22@gmail.com>
2022-04-10 08:35:17 +0300

    added heroku information

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
@@ -9,3 +9,4 @@ app.get('/', (request, response) => {
 })
 
 app.listen(port);
+console.log(`server is listening on port ${port}`);
